285c32c9197ad260bce60c1c38efa70bdbea1345
WasimSamirArsad <wasimakhtar12091@gmail.com>
2024-10-03 15:14:15 +0530

    project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup ,find_packages
+
+setup(
+    name="movie recommendation system",
+    author="wasim akhtar",
+    author_email="wasimcode12091@gmail.com",
+    version="0.0.1",
+    install_requirements='requirements.txt',
+    packages=find_packages()
+)
